58acee1053166d7b9faa4e7430186e97890b4ecd
DanielPohl <pohl.daniel@freenet.de>
2023-03-17 20:11:32 +0100

    add SR-Translator

---
+++ source/transl/pch.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#ifdef _WIN32
+#include <tchar.h>
+#endif
+#include <stdio.h>
+
+#include <iostream>
+#include <fstream>
+
+#include <iomanip>
+#include <ctime>
+
+
+#include <string>
+#include <sstream>
+
+#include <vector>
+#include <map>
+
+#include <boost/filesystem.hpp>
